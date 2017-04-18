b1ed668abb6e1d1ad1770446d5815d219973affe
qba <qba@qba.pl>
2017-04-18 10:12:01 +0200

    dodanie pluginów: imagemin oraz watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,31 @@ module.exports = function(grunt) {
           'style.css': 'style.sass'
         }
       }
-    }
+    },
+	imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
+    },
+	watch: {
+    scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    } 
+}
+}
   });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
   grunt.registerTask('default', ['sass']);
 };
